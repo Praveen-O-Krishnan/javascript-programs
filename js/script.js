eab6b42d7c08b1b5ce6dd19be227b9b7ee33cfc4
Praveen-Krishnan <Praveen.Krishnan@smith.co>
2017-06-05 18:08:35 +0530

    add row and contents in a table

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ function addRow() {
   var row = tbl.insertRow(0);
   var cell1 = row.insertCell(0);
 }
+
+function addContent() {
+  var strng = prompt('Enter the value to add: ', '');
+  var tbl = document.getElementById("table");
+  var row = tbl.insertRow(0);
+  var cell1 = row.insertCell(0);
+  for(var i=1; i<=tbl.rows.length; i++) {
+    if(tbl.rows.length !== 0) {
+      cell1.innerHTML = strng;
+    }
+  }
+}
